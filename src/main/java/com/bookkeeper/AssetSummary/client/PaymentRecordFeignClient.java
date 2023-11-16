0548777c49c64c664edbf6bc501a1a09422f4725
CedricT <cedric.sm.tse@gmail.com>
2023-11-16 23:41:18 +0800

    Rename Feign Client application name

--- src/main/java/com/bookkeeper/AssetSummary/client/PaymentRecordFeignClient.java
+++ src/main/java/com/bookkeeper/AssetSummary/client/PaymentRecordFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("PAYMENTRECORD")
+@FeignClient("PAYMENT-RECORD")
 public interface PaymentRecordFeignClient {
 
     @GetMapping(value = "/api/v1/paymentRecord/{year}/{month}")
